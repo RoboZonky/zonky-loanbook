d6c3465534679470e708ca073ee83d854285f85b
Lukáš Petrovický <lukas@petrovicky.net>
2019-07-08 21:59:31 +0200

    Introduce some more summary charts

---
+++ src/main/java/com/github/robozonky/loanbook/charts/SummaryPurposeTimelineChart.java
@@ -0,0 +1,84 @@
+package com.github.robozonky.loanbook.charts;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Collection;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.stream.Stream;
+
+import com.github.robozonky.loanbook.input.Data;
+import com.github.robozonky.loanbook.input.DataRow;
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
+
+public final class SummaryPurposeTimelineChart extends AbstractTimelineXYZChart {
+
+    public SummaryPurposeTimelineChart(final Data data) {
+        super(data, SummaryPurposeTimelineChart::regionTimeline);
+    }
+
+    private static Number convert(final String purpose, final List<DataRow> rows) {
+        final long total = rows.size();
+        final long part = rows.stream().filter(r -> r.getPurpose().equals(purpose)).count();
+        return BigDecimal.valueOf(part)
+                .divide(BigDecimal.valueOf(total), 4, RoundingMode.HALF_EVEN)
+                .multiply(BigDecimal.TEN)
+                .multiply(BigDecimal.TEN);
+    }
+
+    private static Tuple2<String, Function<List<DataRow>, Number>> convert(final String purpose) {
+        return Tuple.of(purpose, data ->  convert(purpose, data));
+    }
+
+    private static void regionTimeline(final Stream<DataRow> data, final XYZChartDataConsumer adder) {
+        final SortedMap<String, List<DataRow>> all =
+                data.collect(
+                        collectingAndThen(
+                                groupingBy(DataRow::getPurpose,
+                                           toList()),
+                                TreeMap::new
+                        ));
+        final Tuple2<String, Function<List<DataRow>, Number>>[] series = all.keySet().stream()
+                .map(SummaryPurposeTimelineChart::convert)
+                .toArray((IntFunction<Tuple2<String, Function<List<DataRow>, Number>>[]>) Tuple2[]::new);
+        abstractTimeline(all.values().stream().flatMap(Collection::stream), adder, series);
+    }
+
+    @Override
+    public StackingType getStacking() {
+        return StackingType.ABSOLUTE;
+    }
+
+    @Override
+    public boolean isRatingsAsSeries() {
+        return false;
+    }
+
+    @Override
+    public ChartType getType() {
+        return ChartType.COLUMN;
+    }
+
+    @Override
+    public String getLabelForY() {
+        return "Učel úvěru";
+    }
+
+    @Override
+    public String getLabelForZ() {
+        return "Podíl počtu na celku [%]";
+    }
+
+    @Override
+    public String getTitle() {
+        return "Skladba účelu ǘvěrů v čase";
+    }
+}
